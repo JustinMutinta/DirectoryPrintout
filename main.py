219de1327533956b83de996703d1d3c17f962d75
MUTINTA <JustinMutinta@hotmail.com>
2021-08-20 17:05:49 -0500

    3rd Pass. Able to print first and second subfolders respectively. Also added notes

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ directory_contents = os.listdir(path)       #create a variable that will have al
 print(directory_contents)                   #for this example, simply print out the contents of directory_contents
 
 for files in directory_contents:            #decided to use a for-loop to make the contents more presentable.
-    print("-->", files)
-    try:
-        word = path + "/" + files
-        print(os.listdir(word))
-    except:
-        print("nothing here")
+    print("-->", files)                     #for first sub-folder, it will be --> and then the folder name
+    word = path + "/" + files               #the sub folder will be added to the path of the original
+
+    try:                                    #adding a try method because sometime the folder will need authorisations and will not work
+        sub_directory_contents = os.listdir(word)
+        for sub_files in sub_directory_contents:
+            print("----->",sub_files)
+    except:                                 #If access is not allowed or if it is a file, "Nothing here" will be displayed
+        print("-----> nothing here")
 
 
